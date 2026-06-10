8d1a112eeeb0037a64736a719e81af77139d1a8b
shirshance <61277252+shirshance@users.noreply.github.com>
2026-06-10 12:07:48 +0300

    Add files via upload

---
+++ mt_codon_table.py
@@ -0,0 +1,24 @@
+
+
+
+CODON_TABLE = {
+    'TTT': 'F', 'TTC': 'F', 'TTA': 'L', 'TTG': 'L',
+    'CTT': 'L', 'CTC': 'L', 'CTA': 'L', 'CTG': 'L',
+    'ATT': 'I', 'ATC': 'I', 'ATA': 'M', 'ATG': 'M',
+    'GTT': 'V', 'GTC': 'V', 'GTA': 'V', 'GTG': 'V',
+    'TCT': 'S', 'TCC': 'S', 'TCA': 'S', 'TCG': 'S',
+    'AGT': 'S', 'AGC': 'S', 'CCT': 'P', 'CCC': 'P',
+    'CCA': 'P', 'CCG': 'P', 'ACT': 'T', 'ACC': 'T',
+    'ACA': 'T', 'ACG': 'T', 'GCT': 'A', 'GCC': 'A',
+    'GCA': 'A', 'GCG': 'A', 'TAT': 'Y', 'TAC': 'Y',
+    'CAT': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q',
+    'AAT': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K',
+    'GAT': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
+    'TGT': 'C', 'TGC': 'C', 'TGG': 'W', 'TGA': 'W',
+    'CGT': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R',
+    'GGT': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G',
+    'TAA': '_',
+    'TAG': '_',
+    'AGA': '_',
+    'AGG': '_'
+}
